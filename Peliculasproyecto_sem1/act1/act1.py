a444efb3fa6e780031f27ba123c271465031c9bf
Valentin Gimenez <valebertuni@hotmail.com>
2022-08-10 09:16:23 -0300

    intentando ejercicio 1

--- Peliculasproyecto_sem1/act1/act1.py
+++ Peliculasproyecto_sem1/act1/act1.py
@@ -5,4 +5,10 @@ Created on Wed Aug 10 08:51:25 2022
 @author: valeb
 """
 
-print (1)
+archi=open('frases_de_peliculas.txt', 'r')
+info=archi.readlines()
+
+for linea in info:
+    frase, pelicula = linea.split(';')
+    print(frase)
+    print(pelicula)
