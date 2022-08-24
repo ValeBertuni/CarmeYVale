d7756a1facedc4c197e270ee867b6025c9a87dbc
Carme <carmedominguez2003@gmail.com>
2022-08-24 10:16:54 -0300

    actualizo frutas.

--- TP2/Analizador de frutas/main.py
+++ TP2/Analizador de frutas/main.py
@@ -1,7 +1,13 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Aug 24 09:06:13 2022
+#1)analizador de contenidos de nutrientes y agua en frutas.
+#2)cargar su masa mediante consola.
+#3)generar un menú de opciones por consola; naranjas, pomelos y manzanas.
+# debería poder extenderse fácilmente
+#4)datos de las frutas deben poder almacenarse en archivos de 
+#diferente tipo(CSV y JSON). El nombre de los archivos se genera en forma 
+#automática con la fecha y hora actuales(módulo datetime)
+#5)con el dato de la masa de la fruta obtener porcentajes de agua y tres 
+#principales componentes
+#6)debe permitir calcular e informar el contenido total de agua para la 
+# lista de frutas cargadas
 
-@author: carme
-"""
 
---
+++ TP2/Analizador de frutas/modulos/classes.py
@@ -0,0 +1,8 @@
+
+class frutas:
+    def naranja (self, p_nombre, p_masa):
+        pass
+    def pomelo (self, p_nombre, p_masa):
+        pass
+    def manzana (self, p_nombre, p_masa):
+        pass
